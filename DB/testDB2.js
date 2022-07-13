fab01fe2a9c11ed585228b4d35ea4e136bed1b97
Daniele Sponta <56098983+Minho23@users.noreply.github.com>
2022-07-13 12:17:12 +0200

    commit

--- DB/testDB2.js
+++ DB/testDB2.js
@@ -1,21 +1,19 @@
 var ibmdb = require('ibm_db');
-var connStr = "DRIVER={DB2};DATABASE="";HOSTNAME="";UID="";PWD="";PORT=50000;PROTOCOL=TCPIP";
-var check = 1
+var connStr = "DRIVER={DB2};DATABASE="";HOSTNAME="";UID="";PWD="";PORT="";PROTOCOL=TCPIP";
 
 ibmdb.open(connStr, function (err, connection) {
   if (err) {
     console.log(err);
     check = 0
     console.log("ERRORE")
-    console.log(check)
     return;
   }
 
-  connection.query("select * from """, function (err, rows, sqlca) {
+  connection.query("select * from "" ", function (err, rows, sqlca) {
     if (err) {
       console.log(err);
     } else {
-      console.log(rows); // rows = [ [ { '1': 1 } ], [ { '1': 2 } ], [ { '1': 3 } ] ]
+      console.log(rows);
     }
   });
 
@@ -23,5 +21,4 @@ ibmdb.open(connStr, function (err, connection) {
     if (err2) console.log(err2);
   });
 
-  console.log(check)
 });
